63e4ce3f6af03264f9589c3ffe9fe82e9b624b2b
Katie Poeltler <kpoeltler@gmail.com>
2018-04-22 00:58:34 -0500

    functions for updating the console and guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -40,4 +40,74 @@ let gameElements = {
     gameOver:document.querySelector("#game_over"),
     promptToStart:document.querySelector("#prompt_to_start"),
     incorrectGuess:document.querySelector("#incorrect_guess"),
+
+resetWord: function(){
+    this.word.innerHTML = gameStats.createConsole ();
+    this.remainingGuesses.innerText = gameStats.remainingGuesses;
+    this.lettersGuessed.innerText = gameStats.lettersGuessed;
+},
+
+updateLetterCorrect: function() {
+    this.word.innerHTML = gameStats.createConsole ();
+    this.lettersGuessed.innerText = gameStats.lettersGuessed.join(",").toUpperCase();
+},
+
+displayAlerts: function() {
+    this.gameOver.style.display = "block";
+    this.promptStart.style.display = "block";
+},
+
+displayAnswer: function() {
+    function createAnswer() {
+        gameStats.chosenWord.forEach(function(space){
+            answer += "<div class='letter_space'>" + space.toUpperCase() + "</div>";
+        });
+        return answer;
+    }
+    this.word.innerHTML = createAnswer();
+  }
+}
+
+gameSetUp => {
+    gameElements.prompStart.style.display = "none";
+    gameElements.gameOver.style.display = "none";
+    gameStats.resetStats();
+    gameElements.resetWord();
+    window.removeEventListener("keyup", gameSetUp);
+    window.addEventListener("keyup", playGame);
+}
+
+processGuess=(guess)=>{
+    if (gameStats.chosenWord.includes(guess) && ! gameStats.lettersGuessed.includes (guess)) {
+        let arrOfIndex = [];
+        gameStats.chosenWord.forEach(function(letter, index){
+            if (letter === guess) {
+         arrOfIndex.push(index);
+            }
+        });
+        arrOfIndex.forEach(function(i){
+            gameStats.display[i] = guess;
+        });
+        gameStats.lettersGuessed.push(guess);
+        gameElements.updateLetterCorrect();
+    } else {
+        if (!gameStats.lettersGuessed.includes(guess)) {
+            gameStats.remainingGuesses--;
+            gameStats.lettersGuessed.push(guess);
+            gameElements.updateLetterIncorrect();
+        } else {
+            gameElemetns.incorrectEntry.innerText = guess.toUpperCase() + " has been guessed";
+            gameElements.incorrectEntry.style.display = "block";
+        }
+    }
+}
+ 
+     compareLetter = event => {
+         let alphaNumeric = /^[0-9a-zA-Z]+$/;
+         let letterGuess = "";
+         
+     }
+
+
+
 }
