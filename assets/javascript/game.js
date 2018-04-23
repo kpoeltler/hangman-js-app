52f31d6e29cd6ec3b3b2f4687f461166aa8ec75f
Katie <email@example.com>
2018-04-23 00:42:54 -0500

    input touch event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -53,10 +53,10 @@ $('button').on('click', function () { // trigger touch on element to set focus
     focused.next('input').trigger('touchstart'); // trigger touchstart
 });
 
-$('input').on('touchstart', function () {
-    $(this).focus();   // inside this function the focus works
-    focused = $(this); // to point to currently focused
-});
+// $('input').on('touchstart', function () {
+//     $(this).focus();   // inside this function the focus works
+//     focused = $(this); // to point to currently focused
+// });
 
 
 var gameElements = {
